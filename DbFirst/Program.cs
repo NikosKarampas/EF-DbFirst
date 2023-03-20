d1ee0beffa5ef901e5cb14c7d404371634369188
Nikos Karampas <nikos.karampas@hotmail.com>
2023-03-20 12:52:27 +0200

    Database-First implementation

---
+++ DbFirst/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DbFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var course = new Course();
+            course.Level = CourseLevel.Beginner;
+        }
+    }
+}
---
+++ VideoRentalStore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VideoRentalStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add videos using the stored procedure spAddVideo
+            var videoRentalDbContext = new VideoRentalStoreDbContext();
+            //videoRentalDbContext.Database.ExecuteSqlCommand("TRUNCATE TABLE [Videos]");
+
+            videoRentalDbContext.AddVideo("video1", DateTime.Now.AddDays(-130), "Action", (byte)Classification.Gold);
+            videoRentalDbContext.AddVideo("video2", DateTime.Now.AddDays(-120), "Romance", (byte)Classification.Silver);
+            videoRentalDbContext.AddVideo("video3", DateTime.Now.AddDays(-110), "Horror", (byte)Classification.Platinum);
+        }
+    }
+}
